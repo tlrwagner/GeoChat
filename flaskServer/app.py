ff2c40439f94569169e7a5f41bce45252dbb4e74
Chris Bednar <bednar.christopher@gmail.com>
2017-11-12 01:17:27 -0500

    better queries

--- flaskServer/app.py
+++ flaskServer/app.py
@@ -1,8 +1,10 @@
 import MySQLdb
 from flask import Flask
 from flask import jsonify
+import json
+from flask import request
+
 app = Flask(__name__)
-app.debug = True
 
 def query(query, isUpdate=False):
 	db = MySQLdb.connect(host="hackitaldb.ce1doaue7cqd.us-east-1.rds.amazonaws.com", user="hackathon", passwd="thetatau", db="innodb")
@@ -27,16 +29,51 @@ def query(query, isUpdate=False):
 
 #return returnResults
 
+#@app.route('/createEntry', methods=["POST", "GET"])
+#def createEntry():
+#	query("INSERT INTO entry (`entry_text`, `group_id`, `created_user`) VALUES ('" + request.form["test"] + "', 1, 1);", True)
+
 @app.route('/allentrys', methods=["POST", "GET"])
 def entries():
-	returnResults = ""
-	results = query("select * from entry")
-	return jsonify(results)
+	returnResults = []
+	results = query("select u.user_name, e.entry_text from entry e left join user u on (e.created_user = u.user_id)")
+	for result in results:
+		returnResults.append((result[0], result[1]))
+
+	return jsonify({"results": returnResults})
 
 @app.route('/allgroups', methods=["POST", "GET"])
 def groups():
-	returnResults = ""
+	returnResults = []
 	results = query("select * from group")
+	for result in results:
+		returnResults.append(("Chris", result[1]))
+
+	return jsonify(results)
+
+# get the feed for a particular user (only groups the user wants to see posts about)
+@app.route('/userentrys', methods=["POST", "GET"])
+def feedByUser():
+	if (request.method == 'POST'):
+		user_id = request.form["user_id"]
+	else:
+		user_id = 1
+
+	returnResults = []
+	results = query("SELECT u.user_name, e.entry_text FROM entry e left join user u on (e.created_user = u.user_id) where group_id in (select group_id from user_group where user_id = " + str(user_id) + ");")
+	for result in results:
+		returnResults.append(("Chris", result[1]))
+
+	return jsonify(results)
+
+# get the feed for a particular group
+@app.route('/groupentrys', methods=["POST", "GET"])
+def feedByGroup():
+	returnResults = []
+	results = query("SELECT * FROM entry where group_id = 1;")
+	for result in results:
+		returnResults.append(("Chris", result[1]))
+
 	return jsonify(results)
 
 @app.route('/test', methods=['POST'])
